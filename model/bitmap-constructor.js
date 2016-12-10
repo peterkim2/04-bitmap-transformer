8d006edb7e8debbe2fca316aba674a1213057ecb
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-10 14:10:41 -0800

    hot fix to change back to readInt32LE

--- model/bitmap-constructor.js
+++ model/bitmap-constructor.js
@@ -22,21 +22,21 @@ function bitmap(file, callback, color) {
 function BufferData(data) {
   this.buffer = data;
   this.id = data.toString('utf-8', 0, 2);
-  this.size = data.readInt8(2);
+  this.size = data.readInt32LE(2);
   this.unused1 = data.toString('hex', 6, 8);
   this.unused2 = data.toString('hex', 8, 10);
-  this.offset = data.readInt8(10);
-  this.dibheader = data.readInt8(14);
-  this.width = data.readInt8(18);
-  this.height = data.readInt8(22);
-  this.plane = data.readUInt8(26);
-  this.bitsperpixel = data.readInt8(28);
-  this.bi_rbg = data.readInt8(30);
-  this.rawsize = data.readInt8(34);
-  this.pixel_horizontal = data.readInt8(38);
-  this.pixel_vertical = data.readInt8(42);
-  this.number_colors = data.readInt8(46);
-  this.important_colors = data.readInt8(50);
+  this.offset = data.readInt32LE(10);
+  this.dibheader = data.readInt32LE(14);
+  this.width = data.readInt32LE(18);
+  this.height = data.readInt32LE(22);
+  this.plane = data.readUInt32LE(26);
+  this.bitsperpixel = data.readInt32LE(28);
+  this.bi_rbg = data.readInt32LE(30);
+  this.rawsize = data.readInt32LE(34);
+  this.pixel_horizontal = data.readInt32LE(38);
+  this.pixel_vertical = data.readInt32LE(42);
+  this.number_colors = data.readInt32LE(46);
+  this.important_colors = data.readInt32LE(50);
   this.colorArray = data.slice(54, 1078);
   this.pixels = data.slice(1078, 11077);
 }
