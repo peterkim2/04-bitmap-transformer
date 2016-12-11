f0c01611fcccfa76ce5e4051353295724e38f07d
Peter Kim <peterkim23@gmail.com>
2016-12-10 22:11:20 -0800

    Included switch statement to run CLI transform flags

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const readFileHelper = require('./lib/bitmap-file-helper.js');
 
 module.exports = exports = {};
 
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'green');
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'white');
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'invert');
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'greyscale');
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'luminosity');
-readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'black');
+switch (process.argv[2]) {
+case 'green':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'green');
+  break;
+case 'white':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'white');
+  break;
+case 'invert':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'invert');
+  break;
+case 'greyscale':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'greyscale');
+  break;
+case 'luminosity':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'luminosity');
+  break;
+case 'black':
+  readFileHelper.bitmap(readFileHelper.filePath, readFileHelper.callback, 'black');
+  break;
+default:
+  console.log('Please enter one of the following: green, white, invert, grescale, luminosity, black');
+}
 
 exports.convert = process.argv[2];
--- test/bitmap-constructor-test.js
+++ test/bitmap-constructor-test.js
@@ -18,4 +18,5 @@ describe('Testing constructor', function() {
       });
     });
   });
+  
 });
